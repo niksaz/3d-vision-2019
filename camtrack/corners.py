c7a4de87d312dc78761bb6955980fa5deb663bda
miksaz <nikitasazanovich@gmail.com>
2019-02-03 21:40:56 +0300

    Corner tracking implemented.

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -36,10 +36,10 @@ class _CornerStorageBuilder:
 
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    radius = 10
+    radius = 15
     # params for ShiTomasi corner detection
-    feature_params = dict(maxCorners=500,
-                          qualityLevel=0.03,
+    feature_params = dict(maxCorners=1000,
+                          qualityLevel=0.1,
                           minDistance=7,
                           blockSize=7)
     # params for lucas kanade optical flow
@@ -59,37 +59,32 @@ def _build_impl(frame_sequence: pims.FramesSequence,
     image_0_8bit = np.uint8(image_0_8bit * 255.0)
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
         image_1_8bit = np.uint8(image_1 * 255.0)
-        p1, st, err = cv2.calcOpticalFlowPyrLK(image_0_8bit, image_1_8bit, centers, None, **lk_params)
-        same_ids = []
-        for i in range(len(ids)):
-            if st[i] == 1:
-                same_ids.append(i)
+        p1, _st, _err = cv2.calcOpticalFlowPyrLK(image_0_8bit, image_1_8bit, centers, None, **lk_params)
+        p0r, _st, _err = cv2.calcOpticalFlowPyrLK(image_1_8bit, image_0_8bit, p1, None, **lk_params)
+        d = abs(centers - p0r).reshape(-1, 2).max(-1)
+        present_fs = d < 1
 
-        if p1 is not None:
-            ids = ids[same_ids]
-            centers = centers[same_ids]
-            radiuses = radiuses[same_ids]
-            centers = p1[same_ids]
+        ids = ids[present_fs]
+        centers = p1[present_fs]
+        radiuses = radiuses[present_fs]
 
         if len(centers) < feature_params['maxCorners']:
-            p0 = cv2.goodFeaturesToTrack(image_1, mask=None, **feature_params)
-            new_ids = []
-            new_centers = []
-            new_radiuses = []
-            for arr in p0:
-                if len(ids) + len(new_ids) >= feature_params['maxCorners']:
-                    break
-                closest_dst = None
-                for center in centers:
-                    dst = np.linalg.norm(center[0] - arr[0])
-                    if closest_dst is None or dst < closest_dst:
-                        closest_dst = dst
-                if closest_dst > feature_params['minDistance']:
+            mask = np.full(image_1.shape, 255, dtype=np.uint8)
+            for arr in centers:
+                x, y = arr[0]
+                cv2.circle(mask, (x, y), feature_params['minDistance'], 0, -1)
+            p0 = cv2.goodFeaturesToTrack(image_1, mask=mask, **feature_params)
+            if p0 is not None:
+                new_ids = []
+                new_centers = []
+                new_radiuses = []
+                for arr in p0:
+                    if len(ids) + len(new_ids) >= feature_params['maxCorners']:
+                        break
                     new_ids.append(next_id)
                     new_centers.append(arr)
                     new_radiuses.append(radius)
                     next_id += 1
-            if len(new_ids) > 0:
                 ids = np.concatenate([ids, new_ids])
                 centers = np.concatenate([centers, new_centers])
                 radiuses = np.concatenate([radiuses, new_radiuses])
